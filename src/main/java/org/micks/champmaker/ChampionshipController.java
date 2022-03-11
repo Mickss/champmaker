0fa814bae1c42044eca9d16fc0969f09f80f8381
Michał Guz <michal.guz.85@gmail.com>
2022-03-11 21:01:02 +0100

    Firebase+

--- src/main/java/org/micks/champmaker/ChampionshipController.java
+++ src/main/java/org/micks/champmaker/ChampionshipController.java
@@ -1,16 +1,18 @@
 package org.micks.champmaker;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class ChampionshipController {
 
     @GetMapping
     public List<String> getChampionships(){
-        return List.of("Turniej Puffa", "Uwaga turniej", "Czarna kompania turniej");
+        return List.of("Turniej Puffa", "Uwaga Turniej", "Czarna kompania turniej");
     }
 }
