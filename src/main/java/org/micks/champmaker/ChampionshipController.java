7f07128de96d72f2e7ccc2c672717e136e862645
Michał Guz <michal.guz.85@gmail.com>
2022-04-03 12:34:23 +0200

    Post with Firebase

--- src/main/java/org/micks/champmaker/ChampionshipController.java
+++ src/main/java/org/micks/champmaker/ChampionshipController.java
@@ -1,21 +1,19 @@
 package org.micks.champmaker;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -25,22 +23,24 @@ import java.util.stream.Collectors;
 public class ChampionshipController {
 
     @GetMapping
-    public List<String> getChampionships() throws IOException, ExecutionException,InterruptedException {
-        InputStream serviceAccount = new ClassPathResource("db3-champ-firebase-adminsdk-huz5y-f92bad101c.json").getInputStream();
+    public List<Championship> getChampionships() throws IOException, ExecutionException,InterruptedException {
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
-                .setDatabaseUrl("https://db3-champ-default-rtdb.firebaseio.com/")
-                .build();
-
-
-        FirebaseApp.initializeApp(options);
         Firestore db = FirestoreClient.getFirestore();
-
-        CollectionReference championshipsCollection = db.collection("championschips");
+        CollectionReference championshipsCollection = db.collection("championships");
         ApiFuture<QuerySnapshot> championshipsFuture = championshipsCollection.get();
         List<QueryDocumentSnapshot> championships = championshipsFuture.get().getDocuments();
 
-        return championships.stream().map(ch -> ch.getString("champ_name")).collect(Collectors.toList());
+        return championships.stream().map(doc -> {
+           return new Championship(doc.getString("name"));
+//            return doc.getString("champ_name");
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createChampionship(@RequestBody Championship championship) {
+        System.out.println("działa: " + championship.getName());
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference championshipsCollection = db.collection("championships");
+        championshipsCollection.add(championship);
     }
 }
--- src/main/java/org/micks/champmaker/ChampmakerApplication.java
+++ src/main/java/org/micks/champmaker/ChampmakerApplication.java
@@ -3,11 +3,15 @@ package org.micks.champmaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ChampmakerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FirebaseInitializer.initialize();
 		SpringApplication.run(ChampmakerApplication.class, args);
 	}
+
 
 }
