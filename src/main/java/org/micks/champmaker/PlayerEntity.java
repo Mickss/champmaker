8de671c322a0b85c26c200c1a975dbfea7ef8ad9
Michał Guz <michal.guz.85@gmail.com>
2022-07-31 18:07:46 +0200

    Add creating player

---
+++ src/main/java/org/micks/champmaker/PlayerEntity.java
@@ -0,0 +1,33 @@
+package org.micks.champmaker;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "player")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class PlayerEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String playerName;
+    private Long playerNumber;
+    private String playerTeam;
+
+
+    public PlayerEntity(String playerName, Long playerNumber, String playerTeam) {
+        this.playerName = playerName;
+        this.playerNumber = playerNumber;
+        this.playerTeam = playerTeam;
+    }
+}
