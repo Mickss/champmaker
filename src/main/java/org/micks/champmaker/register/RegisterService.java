dd6603ed9064037aa2569d6b0249ab506ff7dec5
Michał Guz <michal.guz.85@gmail.com>
2022-09-14 09:42:02 +0200

    Get teams signed up for the tournament

--- src/main/java/org/micks/champmaker/register/RegisterService.java
+++ src/main/java/org/micks/champmaker/register/RegisterService.java
@@ -3,6 +3,9 @@ package org.micks.champmaker.register;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class RegisterService {
 
@@ -13,5 +16,11 @@ public class RegisterService {
     public void registerTeamToChampionship(Long champId, RegisterDTO registerDTO) {
         RegisterEntity registerEntity = new RegisterEntity(champId, registerDTO.getTeamId(), registerDTO.getRegistrationDate());
         registerRepository.save(registerEntity);
+    }
+
+    public List<Long> getRegisteredTeams(long champId) {
+        List<RegisterEntity> registerEntities = registerRepository.findByChampId(champId);
+        return registerEntities.stream()
+                .map(RegisterEntity::getTeamId).collect(Collectors.toList());
     }
 }
