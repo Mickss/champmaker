816d026440d95cc713d52f2f2cbe59c08b40d524
Mickss <michal.guz.85@gmail.com>
2023-05-14 18:34:45 +0200

    Unit test for filtering championship by city

--- src/test/java/org/micks/champmaker/championships/ChampionshipServiceTest.java
+++ src/test/java/org/micks/champmaker/championships/ChampionshipServiceTest.java
@@ -7,11 +7,15 @@ import org.micks.champmaker.register.RegisterService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,5 +59,35 @@ class ChampionshipServiceTest {
         assertThat(throwable)
                 .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("Can not find Championship with Id: 322");
+    }
+
+    @Test
+    @MockitoSettings(strictness = Strictness.LENIENT)
+    void shouldFilterChampionshipsByCity() {
+
+        // given
+        List<ChampionshipEntity> mockedChampionships = List.of(
+                mockChampionship(301L, "Pomernian", "Szczecin", "1999-01-11"),
+                mockChampionship(302L, "WLU2022", "Warszawa", "1999-01-10"),
+                mockChampionship(303L, "WLU2023", "Warszawa", "1999-01-14"),
+                mockChampionship(304L, "Snejk", "Warszawa Bemowo", "1999-01-12"),
+                mockChampionship(305L, "Puffiaki", "Poznań", "1999-01-18")
+        );
+        when(championshipRepository.findAll()).thenReturn(mockedChampionships);
+
+        // when
+        List<ChampionshipDTO> championshipsInWarsaw = championshipService.getChampionships("Warszawa");
+
+        // then
+        assertThat(championshipsInWarsaw).hasSize(3);
+    }
+
+    private ChampionshipEntity mockChampionship(long id, String name, String city, String date) {
+        ChampionshipEntity mockedChampionship = mock(ChampionshipEntity.class);
+        when(mockedChampionship.getId()).thenReturn(id);
+        when(mockedChampionship.getName()).thenReturn(name);
+        when(mockedChampionship.getCity()).thenReturn(city);
+        when(mockedChampionship.getDate()).thenReturn(date);
+        return mockedChampionship;
     }
 }
