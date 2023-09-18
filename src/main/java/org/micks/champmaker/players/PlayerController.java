d0fa35bf28521f3711abe82e5035ea62f980211b
Michał Guz <michal.guz.85@gmail.com>
2023-09-18 12:37:54 +0200

    Adding properties server port

--- src/main/java/org/micks/champmaker/players/PlayerController.java
+++ src/main/java/org/micks/champmaker/players/PlayerController.java
@@ -4,7 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.micks.champmaker.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
